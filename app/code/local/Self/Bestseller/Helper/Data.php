5f13f4dafe99dcf0b749b76a9177adcf2e1a4a1b
vinh <vinhapthus@gmail.com>
2014-11-14 16:41:12 +0700

    Init Module SELF and add media

---
+++ app/code/local/Self/Bestseller/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: My PC
+ * Date: 14/11/2014
+ * Time: 15:08
+ * To change this template use File | Settings | File Templates.
+ */ 
+class Self_Bestseller_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Self/Zoom/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: My PC
+ * Date: 14/11/2014
+ * Time: 15:10
+ * To change this template use File | Settings | File Templates.
+ */ 
+class Self_Zoom_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
